25abf673490f80ce63dddd344a1f77f199a19eef
xichen9527 <xichen9527@users.noreply.github.com>
2026-07-01 10:21:38 +0800

    fix: set vite base default to /one-man-army-office/ to fix gh-pages 404

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: env.VITE_BASE_PATH || '/',
+    base: env.VITE_BASE_PATH || '/one-man-army-office/',
     plugins: [react()],
     css: {
       postcss: {
